cd92fca59a7e0d055021b5a35a143eaee8731b47
pavlosZakkas <pavloszk93@gmail.com>
2020-07-29 23:31:14 +0300

    Add MaxQuantityServedStrategy
    Strategy is going to:
    - sort the receiver resources by quantity
    - create a max heap of supplier resources based on quantity
    - for each receiver resource from the sorted list, cover its quantity by
    using the top supplier resources from the heap

--- src/main/java/org/crown/service/MaxQuantityServedStrategy.java
+++ src/main/java/org/crown/service/MaxQuantityServedStrategy.java
@@ -1,13 +1,75 @@
 package org.crown.service;
 
+import org.crown.domain.ReceiverResource;
 import org.crown.domain.ResourceMatch;
+import org.crown.domain.ResourceType;
+import org.crown.domain.SupplierResource;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MaxQuantityServedStrategy {
+
+    private static final int ZERO_QUANTITY = 0;
+
     public List<ResourceMatch> generateMatches(ResourceMatchMakingInput resourceMatchMakingInput) {
-        return null;
+
+        List<ResourceMatch> resourceMatches = new ArrayList<>();
+
+        List<ReceiverResource> sortedReceiverResources = resourceMatchMakingInput.getReceiverResources().stream()
+            .sorted(Comparator.comparing(ReceiverResource::getQuantity).reversed())
+            .collect(Collectors.toList());
+
+        MaxHeap<SupplierResource> supplierResourceMaxHeap = new MaxHeap<>(
+            resourceMatchMakingInput.getSupplierResources(),
+            Comparator.comparing(SupplierResource::getQuantity)
+        );
+
+        sortedReceiverResources.forEach(receiverResource -> {
+            Integer receiverQuantity = receiverResource.getQuantity();
+            while (receiverQuantity > 0 && supplierResourceMaxHeap.isNotEmpty()) {
+                SupplierResource supplier = supplierResourceMaxHeap.extractMax();
+                if (receiverQuantity < supplier.getQuantity()) {
+                    resourceMatches.add(resourceMatchOf(resourceMatchMakingInput.getResourceType(), receiverResource, supplier, receiverQuantity));
+                    supplier.setQuantity(supplier.getQuantity() - receiverQuantity);
+                    supplierResourceMaxHeap.insert(supplier);
+                    receiverQuantity = ZERO_QUANTITY;
+                }
+                else {
+                    resourceMatches.add(
+                        resourceMatchOf(
+                            resourceMatchMakingInput.getResourceType(),
+                            receiverResource,
+                            supplier,
+                            supplier.getQuantity()
+                        )
+                    );
+                    receiverQuantity -= supplier.getQuantity();
+                    supplier.setQuantity(ZERO_QUANTITY);
+                }
+            }
+        });
+
+        return resourceMatches;
     }
+
+    private ResourceMatch resourceMatchOf(
+        ResourceType resourceType, ReceiverResource receiverResource,
+        SupplierResource supplierResource,
+        Integer quantity
+    ) {
+        ResourceMatch resourceMatch = new ResourceMatch();
+        resourceMatch.setResourceType(resourceType);
+        resourceMatch.setQuantity(quantity);
+        resourceMatch.setReceiverResource(receiverResource);
+        resourceMatch.setSupplierResource(supplierResource);
+        resourceMatch.setTimestamp(LocalDateTime.now());
+        return resourceMatch;
+    }
+
 }
---
+++ src/test/java/org/crown/service/MaxQuantityServedStrategyTest.java
@@ -0,0 +1,135 @@
+package org.crown.service;
+
+import org.crown.domain.ReceiverResource;
+import org.crown.domain.ResourceMatch;
+import org.crown.domain.ResourceType;
+import org.crown.domain.SupplierResource;
+import org.crown.helper.ReceiverResourceCreator;
+import org.crown.helper.SupplierResourceCreator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MaxQuantityServedStrategyTest {
+
+    private static final ResourceType FACE_SHIELD = new ResourceType().name("Face shield");
+
+    @Autowired
+    private MaxQuantityServedStrategy maxQuantityServedStrategy = new MaxQuantityServedStrategy();
+
+    @Test
+    void gererateMatchedReturnsNoMatchesWhenSuppliersDoNotExist() {
+        List<ReceiverResource> receiverResources = ReceiverResourceCreator.createReceiverResourcesWithQuantities(
+            new ArrayList<>(asList(300, 50, 100)),
+            FACE_SHIELD
+        );
+        List<SupplierResource> supplierResources = SupplierResourceCreator.createSupplierResourcesWithQuantities(
+            new ArrayList<>(emptyList()),
+            FACE_SHIELD
+        );
+
+        ResourceMatchMakingInput resourceMatchMakingInput = inputFrom(receiverResources, supplierResources);
+        List<ResourceMatch> resourceMatches = maxQuantityServedStrategy.generateMatches(resourceMatchMakingInput);
+
+        assertThat(resourceMatches.size()).isEqualTo(0);
+    }
+
+    @Test
+    void generateMatchesWhenRequestedAndSuppliedQuantityMatchOneByOne() {
+        List<ReceiverResource> receiverResources = ReceiverResourceCreator.createReceiverResourcesWithQuantities(
+            new ArrayList<>(asList(300, 50, 100)),
+            FACE_SHIELD
+        );
+        List<SupplierResource> supplierResources = SupplierResourceCreator.createSupplierResourcesWithQuantities(
+            new ArrayList<>(asList(300, 50, 100)),
+            FACE_SHIELD
+        );
+        ResourceMatchMakingInput resourceMatchMakingInput = inputFrom(receiverResources, supplierResources);
+
+        List<ResourceMatch> resourceMatches = maxQuantityServedStrategy.generateMatches(resourceMatchMakingInput);
+
+        assertThat(resourceMatches.size()).isEqualTo(3);
+        assertThat(resourceMatches.get(0).getSupplierResource()).isEqualTo(supplierResources.get(0));
+        assertThat(resourceMatches.get(0).getReceiverResource()).isEqualTo(receiverResources.get(0));
+        assertThat(resourceMatches.get(0).getQuantity()).isEqualTo(300);
+        assertThat(resourceMatches.get(1).getSupplierResource()).isEqualTo(supplierResources.get(2));
+        assertThat(resourceMatches.get(1).getReceiverResource()).isEqualTo(receiverResources.get(2));
+        assertThat(resourceMatches.get(1).getQuantity()).isEqualTo(100);
+        assertThat(resourceMatches.get(2).getSupplierResource()).isEqualTo(supplierResources.get(1));
+        assertThat(resourceMatches.get(2).getReceiverResource()).isEqualTo(receiverResources.get(1));
+        assertThat(resourceMatches.get(2).getQuantity()).isEqualTo(50);
+    }
+
+    @Test
+    void generateMatchesWhenTotalQuantityCannotBeCovered() {
+        List<ReceiverResource> receiverResources = ReceiverResourceCreator.createReceiverResourcesWithQuantities(
+            new ArrayList<>(asList(500, 50, 100)),
+            FACE_SHIELD
+        );
+        List<SupplierResource> supplierResources = SupplierResourceCreator.createSupplierResourcesWithQuantities(
+            new ArrayList<>(asList(300, 50, 100)),
+            FACE_SHIELD
+        );
+        ResourceMatchMakingInput resourceMatchMakingInput = inputFrom(receiverResources, supplierResources);
+
+        List<ResourceMatch> resourceMatches = maxQuantityServedStrategy.generateMatches(resourceMatchMakingInput);
+
+        assertThat(resourceMatches.size()).isEqualTo(3);
+        assertThat(resourceMatches.get(0).getReceiverResource()).isEqualTo(receiverResources.get(0));
+        assertThat(resourceMatches.get(0).getSupplierResource()).isEqualTo(supplierResources.get(0));
+        assertThat(resourceMatches.get(0).getQuantity()).isEqualTo(300);
+        assertThat(resourceMatches.get(1).getReceiverResource()).isEqualTo(receiverResources.get(0));
+        assertThat(resourceMatches.get(1).getSupplierResource()).isEqualTo(supplierResources.get(2));
+        assertThat(resourceMatches.get(1).getQuantity()).isEqualTo(100);
+        assertThat(resourceMatches.get(2).getReceiverResource()).isEqualTo(receiverResources.get(0));
+        assertThat(resourceMatches.get(2).getSupplierResource()).isEqualTo(supplierResources.get(1));
+        assertThat(resourceMatches.get(2).getQuantity()).isEqualTo(50);
+    }
+
+
+    @Test
+    void generateMatchesWhenTotalQuantityIsLessThanTheQuantitySupplied() {
+        List<ReceiverResource> receiverResources = ReceiverResourceCreator.createReceiverResourcesWithQuantities(
+            new ArrayList<>(asList(300, 50, 100)),
+            FACE_SHIELD
+        );
+        List<SupplierResource> supplierResources = SupplierResourceCreator.createSupplierResourcesWithQuantities(
+            new ArrayList<>(asList(200, 40 , 100, 120, 60)),
+            FACE_SHIELD
+        );
+        ResourceMatchMakingInput resourceMatchMakingInput = inputFrom(receiverResources, supplierResources);
+
+        List<ResourceMatch> resourceMatches = maxQuantityServedStrategy.generateMatches(resourceMatchMakingInput);
+
+        assertThat(resourceMatches.size()).isEqualTo(4);
+        assertThat(resourceMatches.get(0).getReceiverResource()).isEqualTo(receiverResources.get(0));
+        assertThat(resourceMatches.get(0).getSupplierResource()).isEqualTo(supplierResources.get(0));
+        assertThat(resourceMatches.get(0).getQuantity()).isEqualTo(200);
+        assertThat(resourceMatches.get(1).getReceiverResource()).isEqualTo(receiverResources.get(0));
+        assertThat(resourceMatches.get(1).getSupplierResource()).isEqualTo(supplierResources.get(3));
+        assertThat(resourceMatches.get(1).getQuantity()).isEqualTo(100);
+
+        assertThat(resourceMatches.get(2).getReceiverResource()).isEqualTo(receiverResources.get(2));
+        assertThat(resourceMatches.get(2).getSupplierResource()).isEqualTo(supplierResources.get(2));
+        assertThat(resourceMatches.get(2).getQuantity()).isEqualTo(100);
+
+        assertThat(resourceMatches.get(3).getReceiverResource()).isEqualTo(receiverResources.get(1));
+        assertThat(resourceMatches.get(3).getSupplierResource()).isEqualTo(supplierResources.get(4));
+        assertThat(resourceMatches.get(3).getQuantity()).isEqualTo(50);
+    }
+
+
+    private ResourceMatchMakingInput inputFrom(List<ReceiverResource> receiverResources, List<SupplierResource> supplierResources) {
+        ResourceMatchMakingInput resourceMatchMakingInput = new ResourceMatchMakingInput();
+        resourceMatchMakingInput.setResourceType(FACE_SHIELD);
+        resourceMatchMakingInput.setSupplierResources(supplierResources);
+        resourceMatchMakingInput.setReceiverResources(receiverResources);
+        return resourceMatchMakingInput;
+    }
+}
